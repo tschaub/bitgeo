d584eb1bd2120c9881a120cbb2c0cc297118aa7b
Tim Schaub <tim.schaub@gmail.com>
2016-09-06 00:35:54 -0700

    Create a subset of the test images

--- test/create-images.js
+++ test/create-images.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const util = require('./util');
 
+const pattern = process.argv[2];
 const fixtures = path.join(__dirname, 'fixtures');
 const images = path.join(__dirname, 'images');
 
 const entries = fs.readdirSync(fixtures);
 
 for (let entry of entries) {
+  if (pattern && entry.indexOf(pattern) === -1) {
+    continue;
+  }
   const factory = require(path.join(fixtures, entry));
   const png = util.createPNG(factory());
   const image = path.join(images, path.basename(entry, '.js') + '.png');
