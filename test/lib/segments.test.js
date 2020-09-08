95ce1aaac9c1855ca31437cd6a6b23fe666f3638
Tim Schaub <tschaub@users.noreply.github.com>
2020-09-08 16:58:16 -0600

    Maintain transformed segment coordinates

--- test/lib/segments.test.js
+++ test/lib/segments.test.js
@@ -6,11 +6,11 @@ const directions = require('../../lib/segments').directions;
 describe('prepareLineString()', () => {
   test('generates flat array of of sorted segments and instructions (bottom to top)', () => {
     const points = [
-      [0, 0],
-      [10, 20],
-      [-5, 30],
-      [5, -10],
-      [20, -5]
+      [0.1, 0.9],
+      [10.1, 20.9],
+      [-5.1, 30.9],
+      [5.1, -10.9],
+      [20.1, -5.9]
     ];
 
     const flattened = prepareLineString(points, 1, [0, 0]);
@@ -18,33 +18,49 @@ describe('prepareLineString()', () => {
     const expected = [
       {
         lowI: 5,
-        lowJ: -10,
+        lowX: 5.1,
+        lowJ: -11,
+        lowY: -10.9,
         highI: 20,
-        highJ: -5,
+        highX: 20.1,
+        highJ: -6,
+        highY: -5.9,
         instruction: 0,
         direction: directions.UP
       },
       {
         lowI: 5,
-        lowJ: -10,
-        highI: -5,
+        lowX: 5.1,
+        lowJ: -11,
+        lowY: -10.9,
+        highI: -6,
+        highX: -5.1,
         highJ: 30,
+        highY: 30.9,
         instruction: 0,
         direction: directions.DOWN
       },
       {
         lowI: 0,
+        lowX: 0.1,
         lowJ: 0,
+        lowY: 0.9,
         highI: 10,
+        highX: 10.1,
         highJ: 20,
+        highY: 20.9,
         instruction: 0,
         direction: directions.UP
       },
       {
         lowI: 10,
+        lowX: 10.1,
         lowJ: 20,
-        highI: -5,
+        lowY: 20.9,
+        highI: -6,
+        highX: -5.1,
         highJ: 30,
+        highY: 30.9,
         instruction: 0,
         direction: directions.UP
       }
